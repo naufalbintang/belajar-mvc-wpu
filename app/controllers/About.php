52328775934eec46c51b64d401ab18e67c0c963f
naufalbintang <naufalbintangpradana@gmail.com>
2026-01-04 16:28:34 +0700

    episode 4

---
+++ app/controllers/About.php
@@ -0,0 +1,14 @@
+<?php
+
+class About
+{
+    public function index(string $nama = "Naufal", string $pekerjaan = "Mahasiswa")
+    {
+        echo "Halo, nama saya $nama, saya adalah seorang $pekerjaan.";
+    }
+
+    public function page()
+    {
+        echo "About/page";
+    }
+}
